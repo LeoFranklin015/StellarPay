08e07039897f8d8083924eebccd4019f4db9d981
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2024-03-03 15:32:01 +0530

    Payment added

--- src/App.jsx
+++ src/App.jsx
@@ -58,14 +58,90 @@ function App() {
         });
       console.log(txResponse);
       console.log("Created the new account", childAccount.publicKey());
+      console.log("Created the new account", childAccount.secret());
+
+      //
+      const account = await server.loadAccount(childAccount.publicKey());
+      console.log("Balances for account: " + childAccount.publicKey());
+      account.balances.forEach(function (balance) {
+        console.log("Type:", balance.asset_type, ", Balance:", balance.balance);
+      });
+      //
     } catch (e) {
       console.error("ERROR!", e);
     }
+  };
+
+  const send = async () => {
+    const server = new StellarSdk.Horizon.Server(
+      "https://horizon-testnet.stellar.org"
+    );
+    var sourceKeys = StellarSdk.Keypair.fromSecret(
+      "SCVZ7G7KJIAUP7XCRAQT6U4P3W74MWVO5SXGMURYJHMH2N7HQJYTFY4R"
+    );
+    var destinationId =
+      "GCOYLNLIFELU6QSUCI5VQ4JKWOWZGK5QCGXAL2BMK2FKFXATXXZ5E5HU";
+    // Transaction will hold a built transaction we can resubmit if the result is unknown.
+    var transaction;
+
+    // First, check to make sure that the destination account exists.
+    // You could skip this, but if the account does not exist, you will be charged
+    // the transaction fee when the transaction fails.
+    server
+      .loadAccount(destinationId)
+      // If the account is not found, surface a nicer error message for logging.
+      .catch(function (error) {
+        if (error instanceof StellarSdk.NotFoundError) {
+          throw new Error("The destination account does not exist!");
+        } else return error;
+      })
+      // If there was no error, load up-to-date information on your account.
+      .then(function () {
+        return server.loadAccount(sourceKeys.publicKey());
+      })
+      .then(function (sourceAccount) {
+        // Start building the transaction.
+        transaction = new StellarSdk.TransactionBuilder(sourceAccount, {
+          fee: StellarSdk.BASE_FEE,
+          networkPassphrase: StellarSdk.Networks.TESTNET,
+        })
+          .addOperation(
+            StellarSdk.Operation.payment({
+              destination: destinationId,
+              // Because Stellar allows transaction in many currencies, you must
+              // specify the asset type. The special "native" asset represents Lumens.
+              asset: StellarSdk.Asset.native(),
+              amount: "1",
+            })
+          )
+          // A memo allows you to add your own metadata to a transaction. It's
+          // optional and does not affect how Stellar treats the transaction.
+          .addMemo(StellarSdk.Memo.text("Test Transaction"))
+          // Wait a maximum of three minutes for the transaction
+          .setTimeout(180)
+          .build();
+        // Sign the transaction to prove you are actually the person sending it.
+        transaction.sign(sourceKeys);
+        // And finally, send it off to Stellar!
+        return server.submitTransaction(transaction);
+      })
+      .then(function (result) {
+        console.log("Success! Results:", result);
+      })
+      .catch(function (error) {
+        console.error("Something went wrong!", error);
+        // If the result is unknown (no response body, timeout etc.) we simply resubmit
+        // already built transaction:
+        // server.submitTransaction(transaction);
+      });
   };
   return (
     <>
       <button onClick={create} className=" bg-green-500">
         Create
+      </button>
+      <button onClick={send} className=" bg-green-500">
+        Send
       </button>
     </>
   );
